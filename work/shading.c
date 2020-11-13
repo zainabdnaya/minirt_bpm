eab3d31832e544ade3d9ae29ef7fe80348fdf595
zainabdnaya <diyanazizo13@gmail.com>
2020-11-13 11:59:52 +0100

    bmp

---
+++ work/shading.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   shading.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zdnaya <zdnaya@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/02 14:29:40 by zdnaya            #+#    #+#             */
+/*   Updated: 2020/11/13 09:50:01 by zdnaya           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minirt.h"
+
+void        shadow_intersection(t_minirt *rt,t_vector shadow_direction,t_vector shadow_origin)
+{
+    if (rt->list_obj->wich_objects == 1 && rt->check != 1)
+        rt->hit_shadow = sphere_equation(rt, shadow_direction,shadow_origin);
+    if (rt->list_obj->wich_objects == 2)
+        rt->hit_shadow = plan_equation(rt, shadow_direction, shadow_origin);
+    if (rt->list_obj->wich_objects == 3 && rt->check != 3)
+        rt->hit_shadow = triangle_equation(rt, shadow_direction,shadow_origin);
+    if (rt->list_obj->wich_objects == 4 && rt->check != 4)
+        rt->hit_shadow = square_equation(rt, shadow_direction,shadow_origin);
+    if (rt->list_obj->wich_objects == 5 && rt->check != 5)
+        rt->hit_shadow = cylindre_equation(rt,shadow_direction, shadow_origin);
+}
+
+
+void       shadow_objects(t_minirt *rt)
+{
+       t_objects *tmp;
+       int i;
+
+       i = 0;
+
+       tmp = NULL;
+        tmp = rt->list_obj;
+        rt->shadow = 0;
+        rt->hit_shadow = 0;
+         while( rt->list_obj != NULL)
+             { 
+                 
+                 shadow_intersection(rt,rt->shadow_direction,rt->shadow_origin);
+                 if (rt->hit_shadow > 0.0)
+                    {
+                            if ( rt->hit_shadow < distance(rt->list_light->position,rt->position))
+                                {
+                                    rt->shadow = 1;
+                                    rt->percent_shadow += 0.4;
+                                    break;
+                                }
+                            else
+                                    rt->shadow = 0;
+                    }
+                rt->list_obj = rt->list_obj->next;
+            }
+        rt->list_obj =  tmp;
+}
+
+
+void       wich_shadow(t_minirt *rt)
+{
+        t_list_lights *lgt;
+        lgt = NULL;
+        lgt = rt->list_light;
+        rt->hit_shadow = 0;
+        rt->shadow = 0;
+        rt->percent_shadow =0;
+        while(rt->list_light != NULL)
+            {
+                rt->shadow_origin.x = rt->position.x + 0.001 * rt->list_obj->light_norm.x;
+                rt->shadow_origin.y = rt->position.y + 0.001 * rt->list_obj->light_norm.y;
+                rt->shadow_origin.z = rt->position.z + 0.001 * rt->list_obj->light_norm.z;
+                rt->shadow_direction.x = rt->list_light->position.x - rt->position.x;
+                rt->shadow_direction.y = rt->list_light->position.y - rt->position.y;
+                rt->shadow_direction.z = rt->list_light->position.z - rt->position.z;
+                rt->shadow_direction = vectorNorme(rt->shadow_direction);
+                shadow_objects(rt);
+                  
+                rt->list_light = rt->list_light->next;
+           }
+        rt->list_light = lgt;        
+}
---
+++ work/vector_3.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector_3.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zdnaya <zdnaya@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/29 21:35:49 by zdnaya            #+#    #+#             */
+/*   Updated: 2020/11/13 09:50:01 by zdnaya           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minirt.h"
+
+t_vector vectorSplit(char *str)
+{
+    t_vector w;
+    char **split;
+
+    split = ft_split(str, ',');
+    if (ft_count(split) != 3 || ft_charCount(str, ',') != 2)
+    {
+        ft_free_split(split);
+        p_error(11);
+        exit(1);
+    }
+    w.x = convert_to_double(split[0]);
+    w.y = convert_to_double(split[1]);
+    w.z = convert_to_double(split[2]);
+    ft_free_split(split);
+    return (w);
+}
+
+t_color  org(t_color r)
+{
+      t_color  result = {r.r / 255, r.g / 255, r.b / 255};
+      return (result);
+}
+
+
+void        initial_all_objects(t_minirt *rt)
+{
+            rt->wich_object = 0;
+            rt->object_number = 0;
+            rt->solution = 0;
+            rt->aspectRatio = 0;
+            rt->half_height = 0;
+            rt->half_width = 0;
+            rt->exist = 0;
+            rt->hit1 = 0;
+            rt->m = 0;
+            rt->check = 0;
+            rt->ratio = 0;
+            rt->hit_shadow = 0;
+            rt->shadow = 0;
+            rt->test = 0;
+            rt->percent_shadow = 0;
+            rt->var = 0;
+            rt->shininess = 0;
+            rt->ks = 0;
+}
